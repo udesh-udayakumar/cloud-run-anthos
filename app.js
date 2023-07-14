441f1b542e9dba149f9aa462c56bde69a1a75ce9
udesh-udayakumar <124115752+udesh-udayakumar@users.noreply.github.com>
2023-07-14 16:11:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 9000;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('This is a simple service \n\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
